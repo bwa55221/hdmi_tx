1644b6fb03f15daf37949e2c9fc66f4b9bb45474
Brandon Ardisson <brandon.ardisson@ui.com>
2023-11-08 12:51:06 -0600

    add hardlinks to synthesis files, add batch script for link generation

--- make_hard_link.bat
+++ make_hard_link.bat
@@ -1,6 +1,19 @@
 ::@echo off
 @echo off
+set repoDir=C:\Users\Brandon\Documents\Git Repositories\hdmi_tx\
+set projDir=C:\Users\Brandon\Documents\Development_Drive\DE10-Nano\091923_hdmi_v0\
+
 echo Creating hard link to project QSF file...
-mklink /h "C:\Users\Brandon\Documents\Git Repositories\hdmi_tx\synthesis\de10_nano_standalone.qsf" "C:\Users\Brandon\Documents\Development_Drive\DE10-Nano\091923_hdmi_v0\de10_nano_standalone.qsf"
+set filename=de10_nano_standalone.qsf
+mklink /h "%repoDir%synthesis\%filename%" "%projDir%%filename%"
+
+echo Creating hard link to project timing constraints...
+set filename=de10_nano_standalone.sdc
+mklink /h "%repoDir%synthesis\%filename%" "%projDir%%filename%"
+
+echo Creating hard link to Platform Designer .qsys file...
+set filename=soc_system.qsys
+mklink /h "%repoDir%synthesis\%filename%" "%projDir%%filename%"
 echo Done creating link
 pause
+
