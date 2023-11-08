a3719b54cac261f3b6184584ae7efc92a8dd1ebb
Brandon Ardisson <brandon.ardisson@ui.com>
2023-11-07 18:02:32 -0600

    adding hard link for .qsf file

---
+++ make_hard_link.bat
@@ -0,0 +1,6 @@
+::@echo off
+@echo off
+echo Creating hard link to project QSF file...
+mklink /h "C:\Users\Brandon\Documents\Git Repositories\hdmi_tx\synthesis\de10_nano_standalone.qsf" "C:\Users\Brandon\Documents\Development_Drive\DE10-Nano\091923_hdmi_v0\de10_nano_standalone.qsf"
+echo Done creating link
+pause
